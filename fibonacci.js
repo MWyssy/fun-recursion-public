bd0edfa05eb88c0e03a252b75e67fb194b0e6c2d
MWyssy <mike@wyss.co.uk>
2023-04-11 16:58:37 +0100

    add fibonacci tests and solution

--- fibonacci.js
+++ fibonacci.js
@@ -1,36 +1,12 @@
-
-function fibonacciF(num1, num2) {
-    let currentNum = num1;
-    let nextNum = num2;
-    let count = 0;
-    function generate() {
-      const result = currentNum + nextNum;
-      if (count ===0) {
-        count++;
-        return num1;
-      }  
-      if (count === 1) {
-        count++;
-        return num2;
-      }
-      currentNum = nextNum;
-      nextNum = result;
-      return result;
-  }
-    return generate
-  }
   
-  
-  function fibonacci(num, result = 0) {
-    const fibonacciSequence = fibonacciF(0, 1)
-    if (num < 2) {
-        console.log(num, result)
-        return result = fibonacciSequence();
+  function fibonacci(num) {
+    let number = num - 1;
+    if (number < 2) {
+      return number;
     }
-    result = fibonacciSequence();
-    const nextStep = num - 1;
-    return fibonacci(nextStep, result);
-};
+    const result = fibonacci(num - 1) + fibonacci(num - 2);
+    return result
+  }
 
 
 module.exports = fibonacci;
